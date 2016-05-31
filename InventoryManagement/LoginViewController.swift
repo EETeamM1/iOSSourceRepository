7c2b719a036803e63fd8c7185586b4bfb6237283
Himanshu Bapna <himanshu.bapna@impetus.co.in>
2016-05-31 19:59:50 +0530

    Implemented login UI screen

---
+++ InventoryManagement/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  InventoryManagement
+//
+//  Created by Himanshu Bapna on 31/05/16.
+//  Copyright © 2016 Impetus. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController : UIViewController {
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func login (){
+        
+    }
+    
+}
