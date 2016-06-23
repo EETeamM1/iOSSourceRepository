95f0ea1cb145bd99086f2a7b6d4485699d6862b2
Himanshu Bapna <himanshu.bapna@impetus.co.in>
2016-06-23 13:28:00 +0530

    Refactor network controller

--- InventoryManagement/NetworkController.swift
+++ InventoryManagement/NetworkController.swift
@@ -13,43 +13,43 @@ class NetworkController: ProtocolNetworkController {
     let serverURL: String = "http://172.26.60.21:9000/InventoryManagement/api"
     
     func sendPostRequest(postData: NSString,  urlString:String, completion: (bool:Bool?, object:NSObject?) -> Void) {
-
-        let urlString = serverURL + urlString
-        let url:NSURL? = NSURL(string: urlString )!
-        let postDataEncoded:NSData = postData.dataUsingEncoding(NSASCIIStringEncoding)!
-        let postLength:NSString = String( postDataEncoded.length )
-        
-        let request:NSMutableURLRequest = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "POST"
-        request.HTTPBody = postDataEncoded
-        request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
-        
-        let session = NSURLSession.sharedSession()
-        session.dataTaskWithRequest(request, completionHandler: getCompletionHandler(completion)).resume()
+        self.sendRequest(postData, urlString: urlString, requestMethod: "POST", completion: completion)
     }
     
     func sendGetRequest(urlString:String, completion: (bool:Bool?, object:NSObject?) -> Void) {
-        
+        self.sendRequest(nil, urlString: urlString, requestMethod: "GET", completion: completion)
+    }
+    
+    func sendRequest(postData:NSString?, urlString:String, requestMethod:String, completion: (bool:Bool?, object:NSObject?) -> Void){
         let urlString = serverURL + urlString
         let url:NSURL? = NSURL(string: urlString )!
+
+        let urlconfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        urlconfig.timeoutIntervalForRequest = 30.0
+        urlconfig.timeoutIntervalForResource = 60.0
+        
         let request:NSMutableURLRequest = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "GET"
+        request.HTTPMethod = requestMethod
+        
+        if requestMethod == "POST"{
+            let postDataEncoded:NSData = postData!.dataUsingEncoding(NSASCIIStringEncoding)!
+            let postLength:NSString = String( postDataEncoded.length)
+            request.HTTPBody = postDataEncoded
+            request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+        }
+       
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("application/json", forHTTPHeaderField: "Accept")
         
-        let session = NSURLSession.sharedSession()
+        let session = NSURLSession(configuration: urlconfig)
         session.dataTaskWithRequest(request, completionHandler: getCompletionHandler(completion)).resume()
     }
     
-    func getCompletionHandler(completion:(bool:Bool?, object:NSObject?) ->Void) -> (NSData?, NSURLResponse?, NSError?) -> Void{
+    func getCompletionHandler(completion:(bool:Bool?, object:NSObject?) ->Void) -> (NSData?, NSURLResponse?, NSError?) -> Void {
         
         let completionHandler: (NSData?, NSURLResponse?, NSError?) -> Void = { (data, response, error) in
             // this is where the completion handler code goes
-            
             if (response != nil) {
-                
                 let httpResponse:NSHTTPURLResponse = response as! NSHTTPURLResponse
                 if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 300) {
                     completion(bool:true, object:data)
